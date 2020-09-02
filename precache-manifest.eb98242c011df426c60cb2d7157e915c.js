55c99d9341310091544eb0371faa773f25e8d3ae
sgproductions <22571003+sgproductions@users.noreply.github.com>
2020-09-02 14:59:55 +0800

    Updates

---
+++ precache-manifest.eb98242c011df426c60cb2d7157e915c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb344d0080d3a55099bb5c51234f3d31",
+    "url": "/console/index.html"
+  },
+  {
+    "revision": "d17c8d73fe5fd04e354c",
+    "url": "/console/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6a966302aff1fbf3a6ce",
+    "url": "/console/static/css/main.67563d12.chunk.css"
+  },
+  {
+    "revision": "d17c8d73fe5fd04e354c",
+    "url": "/console/static/js/2.75010837.chunk.js"
+  },
+  {
+    "revision": "be9831207da3d10befcd05d1ebb51a3a",
+    "url": "/console/static/js/2.75010837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a966302aff1fbf3a6ce",
+    "url": "/console/static/js/main.99476484.chunk.js"
+  },
+  {
+    "revision": "246f400f71fb86eeb83b",
+    "url": "/console/static/js/runtime-main.4f2e123c.js"
+  },
+  {
+    "revision": "83d33d52efaa22e428a12e0094c42bce",
+    "url": "/console/static/media/1.83d33d52.png"
+  },
+  {
+    "revision": "0c25e689993545bae97dd8a8f887dac5",
+    "url": "/console/static/media/2.0c25e689.png"
+  },
+  {
+    "revision": "ef12e49c17b86a6c6fded3653b157bc6",
+    "url": "/console/static/media/3.ef12e49c.png"
+  },
+  {
+    "revision": "2591d8b24cadbfb4ea079dea54e619ec",
+    "url": "/console/static/media/AtlantisInternational-jen0.2591d8b2.ttf"
+  },
+  {
+    "revision": "dafc3934a31a7e4689a8939bc2a38cee",
+    "url": "/console/static/media/HUGE_Button.dafc3934.png"
+  },
+  {
+    "revision": "eadab088cc8dbd02bb2e0c7786bc0c62",
+    "url": "/console/static/media/HUGE_Frame.eadab088.png"
+  },
+  {
+    "revision": "4ec3ddf2a321d4e3acd01becb17469ab",
+    "url": "/console/static/media/lato-v16-latin-900italic.4ec3ddf2.ttf"
+  },
+  {
+    "revision": "55aee0a97fb337fd18114aa1260d6fdd",
+    "url": "/console/static/media/lato-v16-latin-900italic.55aee0a9.eot"
+  },
+  {
+    "revision": "73d7130e1dc196d25bb35faf2afbbb43",
+    "url": "/console/static/media/lato-v16-latin-900italic.73d7130e.woff"
+  },
+  {
+    "revision": "aee0b2e51f3dc857edbf75de430f8c00",
+    "url": "/console/static/media/lato-v16-latin-900italic.aee0b2e5.svg"
+  },
+  {
+    "revision": "bd86aaa72d23c91de85390a93aa6bef2",
+    "url": "/console/static/media/lato-v16-latin-900italic.bd86aaa7.woff2"
+  }
+]);
